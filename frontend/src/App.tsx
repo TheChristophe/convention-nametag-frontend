a6d8507a598d4676b3fecd00385193b9a90c7c76
Christophe <christophe.misc+git@protonmail.ch>
2021-11-24 16:59:06 +0100

    Initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Button, Container } from '@mui/material';
+
+function App() {
+    return (
+        <div className='App'>
+            <Container>
+                <Button variant='contained'>Hello World</Button>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
