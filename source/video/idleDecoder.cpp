9d6c04a94e3b9f98a5b785558d68cbe23b6a6e14
Christophe <christophe.misc+git@protonmail.ch>
2021-11-27 20:41:55 +0100

    [backend] Make IdleDecoder sleep a little to not go too fast

--- source/video/idleDecoder.cpp
+++ source/video/idleDecoder.cpp
@@ -1,6 +1,11 @@
 #include "idleDecoder.hpp"
 
+#include <chrono>
+#include <thread>
+
 void IdleDecoder::DecodeFrame(uint8_t *buffer, int bufferSize)
 {
-    // noop
+    // noop, but don't go too fast
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(10ms);
 }
